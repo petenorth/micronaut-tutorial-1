119e4d4168836697c8bbc98a3d22ab9fdb3a8f61
Peter Fry <peterfry@peters-mbp.mynet>
2019-01-08 13:39:24 +0000

    changes for v1.0.156

--- books/src/main/groovy/example/micronaut/BooksRepositoryImpl.groovy
+++ books/src/main/groovy/example/micronaut/BooksRepositoryImpl.groovy
@@ -10,7 +10,7 @@ class BooksRepositoryImpl implements BooksRepository {
     @Override
     List<Book> findAll() {
         [
-            new Book("1491950358", "Building Microservices v1.0.152"),
+            new Book("1491950358", "Building Microservices v1.0.156"),
             new Book("1680502395", "Release It!"),
         ]
     }
